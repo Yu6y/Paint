3127d63dc704c9275f146e4dac598df5b2332797
Yu6y <126161674+Yu6y@users.noreply.github.com>
2023-03-31 12:12:48 +0200

    Add project files.

---
+++ Paint/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace Paint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonCurve = new System.Windows.Forms.RadioButton();
+            this.radioButtonElipse = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.PB = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PB, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1015, 521);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(194, 515);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonCurve);
+            this.groupBox1.Controls.Add(this.radioButtonElipse);
+            this.groupBox1.Controls.Add(this.radioButtonRectangle);
+            this.groupBox1.Controls.Add(this.radioButtonLine);
+            this.groupBox1.Location = new System.Drawing.Point(0, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 110);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kształt";
+            // 
+            // radioButtonCurve
+            // 
+            this.radioButtonCurve.AutoSize = true;
+            this.radioButtonCurve.Location = new System.Drawing.Point(9, 88);
+            this.radioButtonCurve.Name = "radioButtonCurve";
+            this.radioButtonCurve.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonCurve.TabIndex = 3;
+            this.radioButtonCurve.TabStop = true;
+            this.radioButtonCurve.Text = "Krzywa";
+            this.radioButtonCurve.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonElipse
+            // 
+            this.radioButtonElipse.AutoSize = true;
+            this.radioButtonElipse.Location = new System.Drawing.Point(9, 65);
+            this.radioButtonElipse.Name = "radioButtonElipse";
+            this.radioButtonElipse.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonElipse.TabIndex = 2;
+            this.radioButtonElipse.TabStop = true;
+            this.radioButtonElipse.Text = "Elipsa";
+            this.radioButtonElipse.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(9, 42);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonRectangle.TabIndex = 1;
+            this.radioButtonRectangle.TabStop = true;
+            this.radioButtonRectangle.Text = "Prostokąt";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(9, 19);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(47, 17);
+            this.radioButtonLine.TabIndex = 0;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Linia";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            // 
+            // PB
+            // 
+            this.PB.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PB.Location = new System.Drawing.Point(203, 3);
+            this.PB.Name = "PB";
+            this.PB.Size = new System.Drawing.Size(809, 515);
+            this.PB.TabIndex = 1;
+            this.PB.TabStop = false;
+            this.PB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PB_MouseDown);
+            this.PB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PB_MouseMove);
+            this.PB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PB_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1015, 521);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox PB;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonCurve;
+        private System.Windows.Forms.RadioButton radioButtonElipse;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+    }
+}
+
---
+++ Paint/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Paint
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        List<Point> points;
+        public Form1()
+        {
+            InitializeComponent();
+            PB.Image = new Bitmap(PB.Width, PB.Height);
+            g = Graphics.FromImage(PB.Image);
+        }
+
+        private void PB_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                points = new List<Point>();
+                points.Add(e.Location);
+            }
+            
+        }
+
+        private void PB_MouseUp(object sender, MouseEventArgs e)
+        {
+            Pen myPen = new Pen(Color.Red, 3);
+            if (e.Button == MouseButtons.Left) {
+                points.Add(e.Location);
+                if (radioButtonLine.Checked)
+                {
+                    //Pen myPen = new Pen(Color.Red, 3);
+                    g.DrawLine(myPen, points.First(), e.Location);
+                   
+                }
+                else if (radioButtonRectangle.Checked)
+                {
+                    g.DrawRectangle(myPen,
+                                    Math.Min(points.First().X, points.Last().X),
+                                    Math.Min(points.First().Y, points.Last().Y),
+                                    Math.Abs(points.First().X - points.Last().X),
+                                    Math.Abs(points.First().Y - points.Last().Y));
+                   
+                }
+                else if (radioButtonElipse.Checked)
+                {
+                    g.DrawEllipse(myPen,
+                                    Math.Min(points.First().X, points.Last().X),
+                                    Math.Min(points.First().Y, points.Last().Y),
+                                    Math.Abs(points.First().X - points.Last().X),
+                                    Math.Abs(points.First().Y - points.Last().Y));
+                   
+                }
+                else if (radioButtonCurve.Checked)
+                {
+                    g.DrawCurve(myPen, points.ToArray());
+                    
+                }
+                PB.Refresh();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PB_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(e.Location);
+            }
+
+        }
+    }
+}
